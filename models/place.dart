8abbe5e737be435bbd987fd15675552437107d8a
martirosianartur <martirosianartur4@gmail.com>
2024-08-06 22:16:52 +0200

    add untracked files

---
+++ models/place.dart
@@ -0,0 +1,11 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Place {
+  final String id;
+  final String title;
+
+  Place({required this.title}) : id = uuid.v4();
+}
+// CREATE GIT REPO FOR THIS PROJECT!!!!!!
