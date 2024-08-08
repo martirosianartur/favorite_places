bb5576f60369bdf252e6eb4afc96e2d3031d57bf
martirosianartur <martirosianartur4@gmail.com>
2024-08-08 17:30:09 +0200

    bug fix. use flutter_riverpod instead of riverpod

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,5 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:favorite_places/models/place.dart';
-import 'package:riverpod/riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
