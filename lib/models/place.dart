d1a4e954440fc52408672c7861668360a776db13
martirosianartur <martirosianartur4@gmail.com>
2024-08-13 18:53:47 +0200

    add picked image to the model and "Add Place" form

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
+
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
 class Place {
+  Place(this.image, {required this.title}) : id = uuid.v4();
+
   final String id;
   final String title;
-
-  Place({required this.title}) : id = uuid.v4();
+  final File image;
 }
