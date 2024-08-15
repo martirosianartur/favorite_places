0eaea069aa23e01a2aaaba2cab7d06f51e758aa5
martirosianartur <martirosianartur4@gmail.com>
2024-08-15 11:27:22 +0200

    make the location available from the rest of the app

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -8,7 +8,9 @@ import 'package:http/http.dart' as http;
 import 'package:favorite_places/models/place.dart';
 
 class LocationInput extends StatefulWidget {
-  const LocationInput({super.key});
+  const LocationInput({super.key, required this.onSelectLocation});
+
+  final void Function(PlaceLocation location) onSelectLocation;
 
   @override
   State<LocationInput> createState() => _LocationInputState();
@@ -90,6 +92,7 @@ class _LocationInputState extends State<LocationInput> {
         );
         _isGettingLocation = false;
       });
+      widget.onSelectLocation(_pickedLocation!);
     } catch (error) {
       _showErrorDialog('Something went wrong. Please try again later.');
       setState(() {
